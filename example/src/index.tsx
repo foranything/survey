ac77dec40d04ba341707d9a215c34e484a72f803
ihlee <leeinhwan7@dgist.ac.kr>
2022-12-31 20:58:13 +0900

    fix: create root

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,7 +1,15 @@
 import './index.css'
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const elem = document.getElementById('root');
+if (elem) {
+  const root = ReactDOM.createRoot(elem);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
